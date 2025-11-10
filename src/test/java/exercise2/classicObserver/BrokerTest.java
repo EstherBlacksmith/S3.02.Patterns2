f457d7e4e84491290e695e89d9a6415792cf2d79
Esther <githubesther@gmail.com>
2025-11-10 13:11:44 +0100

    Feat: Test for the classic observer pattern

---
+++ src/test/java/exercise2/classicObserver/BrokerTest.java
@@ -0,0 +1,79 @@
+package exercise2.classicObserver;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrokerTest {
+
+    @Test
+    void update() {
+    }
+    @Test
+    void testBrokerIsUpdatedWhenSubjectChanges(){
+        BrokerAgency brokerAgency = new BrokerAgency();
+
+        Broker  brokerObserver1 = new Broker();
+        Broker  brokerObserver2 = new Broker();
+
+        brokerAgency.addObserver(brokerObserver1);
+        brokerAgency.addObserver(brokerObserver2);
+
+        brokerAgency.getBrokers();
+
+        int oldValue = 5;
+        brokerAgency.setMarketValue(oldValue);
+
+        assert(oldValue ==  brokerObserver1.getMarketValue());
+        assert(oldValue ==  brokerObserver2.getMarketValue());
+
+        int newValue = 10;
+        brokerAgency.setMarketValue(newValue);
+        assert(newValue ==  brokerObserver1.getMarketValue());
+        assert(newValue ==  brokerObserver2.getMarketValue());
+
+    }
+
+    @Test
+    void testBrokerIsNotUpdatedIfDeattached() {
+
+        BrokerAgency brokerAgency = new BrokerAgency();
+
+        Broker  brokerObserver1 = new Broker();
+
+        brokerAgency.addObserver(brokerObserver1);
+
+        brokerAgency.getBrokers();
+
+        int oldValue = 5;
+        brokerAgency.setMarketValue(oldValue);
+
+        assert(oldValue ==  brokerObserver1.getMarketValue());
+
+        int newValue = 10;
+        brokerAgency.removeObserver(brokerObserver1);
+        brokerAgency.setMarketValue(newValue);
+        assertNotEquals(newValue, brokerObserver1.getMarketValue());
+
+    }
+
+    @Test
+    void testBrokerIsDeattachedWhenRemoved() {
+
+        BrokerAgency brokerAgency = new BrokerAgency();
+        Broker brokerObserver1 = new Broker();
+        Broker brokerObserver2 = new Broker();
+
+
+        brokerAgency.addObserver(brokerObserver1);
+        brokerAgency.addObserver(brokerObserver2);
+
+        brokerAgency.removeObserver(brokerObserver1);
+        List<Broker> brokerList2 = brokerAgency.getBrokersList();
+
+        assertThat(brokerObserver1).isNotIn(brokerList2);
+
+    }
+}

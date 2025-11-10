7984be3e02f961aa5fd2681fb43e3e8163ca7258
Esther <githubesther@gmail.com>
2025-11-10 13:10:56 +0100

    Feat: Changes to add test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.assertj:assertj-core:3.26.0")
 }
 
 tasks.test {
